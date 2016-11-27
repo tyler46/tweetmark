87db26dd012435de935b7494c9ce6427d9fd4ddd
spyros <mail.doctor46@gmail.com>
2016-11-27 22:13:34 +0200

    Working on rpc services

--- services/services.py
+++ services/services.py
@@ -1,23 +1,67 @@
-from nameko.rpc import rpc
+from datetime import datetime
+
+from nameko.rpc import rpc, RpcProxy
+import requests
+from requests.exceptions import ConnectionError, RequestException
+from requests.status_codes import codes
+
+import config
+
+VALID_STATUS_CODES = (
+    codes.ok,  # 200
+    codes.created,  # 201
+    codes.no_content,  # 204
+)
+
+TWITTER_DATETIME_FORMAT = '%a %b %d %H:%M:%S %z %Y'
+FAVORITE = 'favorite'
 
 
-def extract_urls(entities):
-    return [item['expanded_url'] for item in entities['urls']]
+def extract_url(entities):
+    return [item['expanded_url'] for item in entities['urls']][0]
 
 
 def get_twitter_public_url(screen_name):
     return 'https://twitter.com/{}'.format(screen_name)
 
 
+class StoreTweetService:
+
+    name = 'store_tweet_service'
+
+    @rpc
+    def send_payload(self, payload):
+        url = config.API_URL
+        try:
+            # TODO: Add some custom authorization header
+            # for somekind of security
+            response = requests.post(url, json=payload)
+            if response.status_code not in VALID_STATUS_CODES:
+                print('error, invalid status code: {}'.format(response.status_code))
+                print('reason: {}'.format(response.content))
+        except (RequestException, ConnectionError) as exc:
+            print('error')
+
+
 class FavoritedTweetService:
 
     name = 'tweet_favorites_service'
 
-    @rpc
-    def process_tweet(self, user, tweet):
-        print('{0} has favorited the following tweet: {1}'.format(user, tweet))
+    api = RpcProxy('store_tweet_service')
 
-        return {
-            'user': user['screen_name'],
-            'fav': tweet['text']
-        }
+    @rpc
+    def process_tweet(self, user, tweet, action):
+        print('{0} has favorited the following tweet: {1}'.format(user, tweet))
+        if action != FAVORITE:
+            # self.api.delete_resource(tweet['id_str'])
+            print('Unvaforited')
+        else:
+            posted_at = datetime.strptime(tweet['created_at'], TWITTER_DATETIME_FORMAT)
+            data = {
+                'link_url': extract_url(tweet['entities']),
+                'posted_at': posted_at.isoformat(),
+                'posted_by': get_twitter_public_url(tweet['user']['screen_name']),
+                'text': tweet['text'],
+                'public_id': tweet['id_str']
+            }
+            self.api.send_payload(data)
