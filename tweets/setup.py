77fc38444959a787dbedfa1a24ae6bfa145ce43b
spyros <mail.doctor46@gmail.com>
2020-04-07 10:07:09 +0300

    Upgate nameko to v3.0.0-rc8
    Nameko version 3 offers global config

--- tweets/setup.py
+++ tweets/setup.py
@@ -6,7 +6,7 @@ setup(
     version="0.0.1",
     description="Receive tweets and store them",
     packages=find_packages(exclude=["test", "test.*"]),
-    install_requires=["nameko==2.12.0", "tweepy==3.8.0", "arrow==0.15.5"],
+    install_requires=["nameko==3.0.0rc8", "tweepy==3.8.0", "arrow==0.15.5"],
     extras_require={"dev": ["pytest==5.4.1", "coverage==5.0.4", "flake8==3.7.9"]},
     zip_safe=True,
 )
--- tweets/tweets/service.py
+++ tweets/tweets/service.py
@@ -1,10 +1,14 @@
+from nameko import config
+
 from tweets.dependencies import receive
 
 
 class TweetService:
     name = "tweets_service"
 
-    @receive(wait_for=60, days_back=15)
+    @receive(
+        wait_for=int(config["TIMELINE_INTERVAL"]), days_back=int(config["TIMELINE_DAYS_BACK"])
+    )
     def handle_timeline_message(self, body):
         print("I have got a new favorited tweet")
         print(body)
